213767acfa6bf5ce0a1e34163eeaf338bdb848c9
itoNano <itoKey120+1@gmail.com>
2020-07-05 13:03:16 +0900

    scss設定

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router' //追加
-require('@/assets/sass/main.scss');
+require('@/assets/scss/main.scss');
 
 Vue.config.productionTip = false
 
