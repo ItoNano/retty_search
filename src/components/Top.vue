606f6601a83d60c35451ff2717e2de523c92aac8
itoNano <itoKey120+1@gmail.com>
2020-07-05 13:11:46 +0900

    textfield導入

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,17 +1,72 @@
 <template>
   <div>
+    <text-field />
   </div>
 </template>
 
 <script>
+import TextField from '@/components/atoms/TextField.vue'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components : {
+    TextField,
+  },
+  data() {
+    return {
+      search_list: {
+        work: [
+          'カメラマン・カメラアシスタント',
+          'ヘア・メイクスタッフ',
+          'コーディネーター',
+          '接客スタッフ',
+          '店長',
+          '販売スタッフ',
+          '着付け師',
+          '経理',
+          '人事',
+          '事務スタッフ',
+          '広報',
+          'マーケティング',
+          'デザイナー',
+          'EC',
+          '営業',
+          '企画',
+          'コールセンター'
+        ],
+        conditions: [
+          '急募求人',
+          '未経験可',
+          '転勤なし',
+          '学歴不問',
+          '語学を活かす',
+          '採用枠5名以上',
+          '年齢20~30代スタッフ多め',
+          '年齢40~60代スタッフ多め',
+          '制服あり',
+          '服装自由',
+          '職種・業界未経験可',
+          '職種・業界経験者歓迎',
+          '転勤なし',
+          'おしゃれな店舗',
+          '昇給・昇格あり',
+          'ブランクOK',
+          '研修制度充実',
+          '福利厚生あり',
+          'インセンティブあり',
+          '家賃補助あり',
+          '交通費支給あり',
+          '車・バイク通勤可',
+          '老舗店舗',
+          '試用期間あり',
+          '在宅ワークあり',
+          'Web面談可能'
+        ]
+      },
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+@import '@/assets/scss/color.scss';
 </style>
---
+++ src/components/atoms/TextField.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <input class="text-field" :type="type" :style="textFieldSize" :value="value" @input="updateValue" @keydown.enter="enterText" />
+    <div v-if="validationMessage !== ''" class="validation-message">{{ validationMessage }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '',
+    },
+    validationMessage: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      textFieldSize: {
+        height: this.height,
+        width: this.width,
+      },
+    }
+  },
+  methods: {
+    updateValue(e) {
+      this.$emit('input', e.target.value)
+    },
+    enterText(e) {
+      if (e.keyCode !== 13) return
+      this.$emit('enter')
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/scss/color.scss';
+div {
+  position: relative;
+}
+.text-field {
+  border-radius: 3px;
+  border: 1px solid $common;
+  height: 40px;
+  font-size: 16px;
+  color: $common;
+  padding: 8px;
+  box-sizing: border-box;
+}
+.validation-message {
+  color: $caution;
+  position: absolute;
+  top: 6px;
+  left: 4px;
+  pointer-events: none;
+}
+.validation-message-type {
+  font-size: 10px;
+  color: $caution;
+  position: absolute;
+  top: 23px;
+  left: 4px;
+  pointer-events: none;
+}
+:focus {
+  outline: 0;
+}
+</style>
