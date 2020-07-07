38abb17fe044b52f2e56be41783814effac238f2
itoNano <itoKey120+1@gmail.com>
2020-07-08 00:18:24 +0900

    検索機能完成

--- src/components/organisms/Modals/SearchModal.vue
+++ src/components/organisms/Modals/SearchModal.vue
@@ -16,6 +16,22 @@
       </div>
     </template>
     <template v-slot:main-content>
+      <div class="result-box">
+        <div class="result">
+          <div>職業:{{resultData.work}}</div>
+          <div>条件:{{resultData.conditions}}</div>
+        </div>
+        <div class="select">
+          <div class="select-box" v-for="(workItem, workIndex) in getSearchData.work" :key="workIndex">
+            <div class="select-item" @click="selectWork(workItem.name)" v-if="resultWork(workItem.name)">{{workItem.name}}</div>
+          </div>
+        </div>
+        <div class="select">
+          <div class="select-box" v-for="(conditionsItem, conditionsIndex) in getSearchData.conditions" :key="conditionsIndex">
+            <div class="select-item" @click="selectConditions(conditionsItem.name)" v-if="resultConditions(conditionsItem.name)">{{conditionsItem.name}}</div>
+          </div>
+        </div>
+      </div>
     </template>
     <template v-slot:footer>
     </template>
@@ -31,16 +47,28 @@ export default {
     Modal,
     TextField,
   },
-  props: {
-    userData: {
-      type: Object,
-      default: () => {},
-    }
-  },
   computed: {
     ...mapGetters({
       getSearchData: 'search/getSearchData',
     }),
+    resultWork() {
+      return function(text){
+        if(text.indexOf(this.inputData.work) !== -1) {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+    resultConditions() {
+      return function(text){
+        if(text.indexOf(this.inputData.conditions) !== -1) {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
   },
   data() {
     return {
@@ -48,14 +76,23 @@ export default {
         work: '',
         conditions: '',
       },
+      resultData: {
+        work: '',
+        conditions: '',
+      }
     }
   },
   mounted() {
-    
   },
   methods: {
     closeModal() {
       this.$emit('closeModal', event)
+    },
+    selectWork(text) {
+      this.resultData.work = text
+    },
+    selectConditions(text) {
+      this.resultData.conditions = text
     },
   },
 }
