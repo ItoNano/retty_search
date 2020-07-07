be56f2329d73df07d9123a232dc241afac1c83fd
itoNano <itoKey120+1@gmail.com>
2020-07-07 22:19:12 +0900

    条件編集モーダル完成

--- src/components/organisms/Modals/EditConditionsModal.vue
+++ src/components/organisms/Modals/EditConditionsModal.vue
@@ -1,70 +1,87 @@
 <template>
-  <modal :content-style='contentStyle'>
+  <modal>
     <template ref='header' v-slot:main-header>
     </template>
     <template ref='content' v-slot:main-content>
-      <div class="button-area">
-        <Button
-          class="modal-button"
+      <div class="content-box">
+        <div class="form" v-for="(conditionsItem, conditionsIndex) in inputSearchData" :key='conditionsIndex'>
+          <div class="form-content">
+            <text-field width="65vw" height="36px" v-model="conditionsItem.name"/>
+            <Button
+              class="modal-footer-button"
+              type="caution"
+              width="50px"
+              height="36px"
+              @click="deleteContent(conditionsIndex)"
+            >
+              削除
+            </Button>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <div>
+        <!-- <Button
+          class="modal-footer-button"
           type="sub"
-          width="320px"
+          width="150px"
           height="42px"
           @click="closeModal"
         >
           キャンセル
-        </Button>
+        </Button> -->
         <Button
-          class="modal-button"
+          class="modal-footer-button"
           type="main"
-          width="320px"
+          width="150px"
           height="42px"
-          @click="closeModal"
+          @click="saveContent"
         >
           保存
         </Button>
       </div>
     </template>
-    <template v-slot:footer>
-    </template>
   </modal>
 </template>
 <script>
 import Modal from '@/components/organisms/Modals/index.vue';
-// import TextField from '@/components/atoms/TextField.vue'
+import TextField from '@/components/atoms/TextField.vue'
 import Button from '@/components/atoms/Button.vue';
-import { mapGetters } from 'vuex'
 
 export default {
   components: {
     Button,
     Modal,
-    // TextField,
+    TextField,
   },
   props: {
-    userData: {
+    getSearchData: {
       type: Object,
       default: () => {},
     }
   },
-  computed: {
-    ...mapGetters({
-      getSearchData: 'search/getSearchData',
-    }),
+  mounted() {
+    const getData = this.$store.getters['search/getSearchData']
+    this.inputSearchData = getData.conditions
   },
   data() {
     return {
-      inputData: {
-        work: '',
-        conditions: '',
-      },
+      inputSearchData: [],
     }
-  },
-  mounted() {
   },
   methods: {
     closeModal() {
+      // this.$store.commit('search/setSearchConditionsData',this.inputSearchData)
       this.$emit('closeModal', event)
     },
+    deleteContent(conditionsIndex) {
+      this.inputSearchData.splice(conditionsIndex, 1)
+    },
+    saveContent() {
+      this.$store.commit('search/setSearchConditionsData',this.inputSearchData)
+      this.$emit('closeModal', event)
+    }
   },
 }
 </script>
@@ -72,5 +89,8 @@ export default {
 @import '@/assets/scss/modal.scss';
 .close-icon {
   padding-right: 8px;
+}
+.box {
+  height: 50px;
 }
 </style>
