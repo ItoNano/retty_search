de509a67241c2868a472faeee213e181dbc90ef0
itoNano <itoKey120+1@gmail.com>
2020-07-10 10:02:04 +0900

    vue-routerのハッシュタグを削除

---
+++ src/components/pages/ToDo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="page">
+    <table>
+      <tr>
+        <th
+          v-for="(headerName, headerIndex) in headerList"
+          :key = "headerIndex"
+        >
+          {{headerName}}
+        </th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+// import strage from '@/mixins/strage.js'
+export default {
+
+  data() {
+    return {
+      headerList: ['No', '', 'コメント', '状態', '-'],
+      recordList: [
+        {
+          things: 'やること1',
+          ing: false,
+        },
+        {
+          things: 'やること2',
+          ing: true,
+        },
+      ],
+      point: ''
+    }
+  },
+  mounted() {
+    // this.recordList.forEach (value)
+    // localStorage.setItem('point', JSON.stringify(this.recordList))
+    // this.point = JSON.parse(localStorage.getItem('point'))
+    // console.log(this.point[1])
+  },
+
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Top from '@/components/Top.vue'
+import Top from '@/components/pages/Top.vue'
+import ToDo from '@/components/pages/ToDo.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       component: Top
     },
+    {
+      path: '/todo',
+      component: ToDo,
+    }
   ]
 })
